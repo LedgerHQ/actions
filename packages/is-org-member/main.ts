bc04f8004940602c179d10b6a54cfc345aa2b9f1
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-01-12 16:32:35 +0100

    fix is-org-member

--- packages/is-org-member/main.ts
+++ packages/is-org-member/main.ts
@@ -5,7 +5,6 @@ function checkStatus(result) {
   if (result.status >= 200 && result.status < 300) {
     return result;
   }
-
   core.setFailed(`Received status ${result.status} from API.`);
   process.exit();
 }
@@ -17,21 +16,47 @@ const main = async function (): Promise<void> {
 
   const octokit = github.getOctokit(token);
 
-  const { data: orgs } = checkStatus(
-    await octokit.rest.orgs.listForUser({
-      username,
-      per_page: 100,
-    })
-  );
-
-  const isMember = orgs.some(
-    ({ login }) => login.toLowerCase() === organisation.toLowerCase()
-  );
-
-  if (!isMember) {
-    core.setFailed(
-      `${username} is not part of the ${organisation} organisation`
+  try {
+    const { data: orgs } = checkStatus(
+      await octokit.rest.orgs.listForUser({
+        username,
+        per_page: 100,
+      })
     );
+
+    let members = [];
+
+    const getAllMembers = async (page = 0) => {
+      const { data: lhq } = await octokit.rest.orgs.listMembers({
+        org: organisation,
+        per_page: 100,
+        page,
+      });
+
+      members = members.concat(lhq.map((member) => member.login));
+
+      if (!lhq || !lhq.length) {
+        return;
+      }
+
+      page++;
+
+      return getAllMembers(page);
+    };
+
+    await getAllMembers();
+
+    const isMember = orgs.some(
+      ({ login }) => login.toLowerCase() === organisation.toLowerCase()
+    );
+
+    if (!isMember) {
+      core.setFailed(
+        `${username} is not part of the ${organisation} organisation`
+      );
+    }
+  } catch (error) {
+    core.setFailed('Error fetching informations');
   }
 };
 
