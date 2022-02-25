20f77d46f419467a1246e92cabebb59ec12b8acc
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-02-25 16:26:56 +0100

    add has-hash-commit-deps

---
+++ packages/has-hash-commit-deps/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core';
+import { join } from 'path';
+import { promises as fs } from 'fs';
+
+async function main() {
+  const workspace = core.getInput('workspace');
+  const pkg = core.getInput('package');
+  const packagePath = join(workspace, pkg);
+  try {
+    const p = await fs.readFile(packagePath, 'utf8');
+    if (!p) {
+      core.setOutput('has-hash-commit-deps', false);
+      return;
+    }
+    const json = JSON.parse(p);
+    const { dependencies = {}, devDependencies = {} } = json;
+    const versions = Object.keys(dependencies)
+      .map((key) => dependencies[key])
+      .concat(Object.keys(devDependencies).map((key) => devDependencies[key]));
+    const has = versions.some((v) => v.includes('.git'));
+    core.setOutput('has-hash-commit-deps', has);
+    return;
+  } catch (error) {
+    core.error(error);
+    throw error;
+  }
+}
+
+main().catch((err) => core.setFailed(err));
