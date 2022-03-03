8ec308d93cc9c7de4651608a8479a908db435712
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-03-03 18:15:51 +0100

    update has-commit-hash-deps

--- packages/has-hash-commit-deps/main.ts
+++ packages/has-hash-commit-deps/main.ts
@@ -1,30 +1,32 @@
 import * as core from '@actions/core';
 import { join } from 'path';
-import { promises as fs } from 'fs';
+import { promises as fs, existsSync } from 'fs';
 
 async function main() {
-  const workspace = core.getInput('workspace');
-  const pkg = core.getInput('package');
-  const packagePath = join(workspace, pkg);
-  core.debug(packagePath);
-  try {
-    const p = await fs.readFile(packagePath, 'utf8');
-    core.debug(p);
-    if (!p) {
-      core.setOutput('has-hash-commit-deps', false);
+  const filePath = join(core.getInput('workspace') || '', 'package.json');
+  core.info(filePath);
+  if (existsSync(filePath)) {
+    try {
+      const p = await fs.readFile(filePath, 'utf8');
+      core.info(p);
+      if (!p) {
+        core.setOutput('has-hash-commit-deps', false);
+        return;
+      }
+      const json = JSON.parse(p);
+      const { dependencies = {}, devDependencies = {} } = json;
+      const versions = Object.keys(dependencies)
+        .map((key) => dependencies[key])
+        .concat(
+          Object.keys(devDependencies).map((key) => devDependencies[key])
+        );
+      const has = versions.some((v) => v.includes('.git'));
+      core.setOutput('has-hash-commit-deps', has);
       return;
+    } catch (error) {
+      core.info(error);
+      throw error;
     }
-    const json = JSON.parse(p);
-    const { dependencies = {}, devDependencies = {} } = json;
-    const versions = Object.keys(dependencies)
-      .map((key) => dependencies[key])
-      .concat(Object.keys(devDependencies).map((key) => devDependencies[key]));
-    const has = versions.some((v) => v.includes('.git'));
-    core.setOutput('has-hash-commit-deps', has);
-    return;
-  } catch (error) {
-    core.debug(error);
-    throw error;
   }
 }
 
